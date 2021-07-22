5e99b5dabe9b8847978933e9ce7b6fd7ef2dc16f
MehrshadSoltan <soltanmehrshad02@gmail.com>
2021-07-22 16:35:57 +0430

    debuged!!!

--- src/app/components/BaseLayout/index.tsx
+++ src/app/components/BaseLayout/index.tsx
@@ -31,27 +31,25 @@ export const BaseLayout = memo(
     const [scrolling, setScrolling] = useState(false);
     const [scrollTop, setScrollTop] = useState(0);
     const [blackColor, setBlackColor] = useState(false);
-    const stickyRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-      // console.log(scrollTop, 'scrollTop');
-      // console.log(scrolling, 'scrolling');
       const onScroll = e => {
         setScrollTop(e.target.documentElement.scrollTop);
         setScrolling(e.target.documentElement.scrollTop < scrollTop);
       };
       window.addEventListener('scroll', onScroll);
 
-      return () => window.removeEventListener('scroll', onScroll);
-    }, [scrollTop, scrolling]);
-
-    useEffect(() => {
-      if (window.scrollY > 20) {
+      if (window.pageYOffset > 20) {
         setBlackColor(true);
       } else {
         setBlackColor(false);
       }
-    }, []);
+      return () => window.removeEventListener('scroll', onScroll);
+    }, [scrollTop, scrolling]);
+
+    // useEffect(() => {
+
+    // }, []);
 
     return (
       <StyledBaseLayout className={`BaseLayout ${className || ''}`}>
